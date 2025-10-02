b9ffca54abeb4b27e1a595c16cf15ff156341408
arnold <arnldrylapuz@gmail.com>
2025-10-02 16:15:30 +0800

    Finished ticketCounter, general admission tickets, free tickets and the total price

--- app/src/main/java/ph/edu/comteq/lapuzarnoldlab3/TicketingActivity.kt
+++ app/src/main/java/ph/edu/comteq/lapuzarnoldlab3/TicketingActivity.kt
@@ -245,6 +245,7 @@ fun Ticketing(modifier: Modifier = Modifier) {
                 fontFamily = playfairdisplayregular,
                 color = Color.Black
             )
+
             Button(
                 modifier = Modifier.padding(5.dp),
                 onClick = { /*todo*/ },
